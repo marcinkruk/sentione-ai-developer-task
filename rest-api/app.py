150f673029e6d6e7d0a1e2ccc9d30ade9fd86928
Marcin Kruk <marcin0kruk@gmail.com>
2022-02-20 19:26:59 +0100

    REST-API imporovements
    - accept only post
    - validate input type
    - respond with JSON
    - rename C++ wrappers and calls

--- rest-api/app.py
+++ rest-api/app.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python
 
-from flask import Flask, jsonify
+import json
+from flask import Flask, jsonify, request
+import polem_enrich
 
 app = Flask(__name__)
 
-@app.route('/sentione/lemmatize', methods=['POST'])
+_polem_enrich = None
+
+@app.before_first_request
+def before_first_request_f():
+    global _polem_enrich
+    _polem_enrich = polem_enrich.Polem_enrich()
+
+@app.route('/sentione/polem-enrich', methods=['POST'])
 def lemmatize():
-    return jsonify({'response': 'LEMMATIZE'})
+    if request.content_type != 'application/json':
+        return "Invalid content type. Expecting application/json.", 415
+
+    j = request.get_json()
+    echo = _polem_enrich.echo(str(j))
+    js = json.loads('"' + echo + '"')
+    return jsonify(j)
 
 app.run()
