639c787e37eac7fe667e9cd73e87d73c01525221
Marcin Kruk <marcin0kruk@gmail.com>
2022-02-21 10:50:53 +0100

    Validate and convert input

--- polem_enrich/src/polem_enrich.cpp
+++ polem_enrich/src/polem_enrich.cpp
@@ -6,7 +6,28 @@
 
 std::string Polem_enrich::enrich(std::string input) {
 
-    json j = json::parse(input);
+    json j_object = input_to_json(input);
+    if(j_object.empty())
+        return "";
 
-    return to_string(j.at("docs"));
+    return to_string(j_object);
+}
+
+json Polem_enrich::input_to_json(std::string input) {
+
+    if(!json::accept(input))
+        return empty_json();
+
+    json j_object = json::parse(input);
+
+    // Given limited test cases input validation is simplified.
+    if(!j_object.contains("docs"))
+        return empty_json();
+
+    return j_object;
+}
+
+inline json Polem_enrich::empty_json() {
+
+    return "{}"_json;
 }
--- polem_enrich/src/polem_enrich.h
+++ polem_enrich/src/polem_enrich.h
@@ -9,6 +9,9 @@ using json = nlohmann::json;
 class Polem_enrich {
     public:
         std::string enrich(std::string);
+    private:
+        json input_to_json(std::string);
+        json empty_json();
 };
 
 #endif
